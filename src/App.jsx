381e9e139dcd1c857182aa1346a9c98092e977fa
Carlos Gámez <hardpx@gmail.com>
2024-08-04 19:22:49 -0600

    dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,5 @@
-import Accordion from './components/Accordion';
-import ButtonPage from './pages/ButtonPage';
-
 function App() {
-  const items = [
-    {
-      id: 'asdfasd',
-      label: 'this is a label',
-      content: 'This is content',
-    },
-    {
-      id: 'fghhr6',
-      label: 'this is a label 2',
-      content: 'This is content 2',
-    },
-    {
-      id: '7rtygh',
-      label: 'this is a label 3',
-      content: 'This is content 3',
-    },
-  ];
-
-  return (
-    <>
-      <h1 className='text-3xl font-bold underline'>Components</h1>
-      <div className='default'>
-        {/* <ButtonPage /> */}
-        <Accordion items={items} />
-      </div>
-    </>
-  );
+  return <div>App</div>;
 }
 
 export default App;
---
+++ src/pages/AccordionPage.jsx
@@ -0,0 +1,32 @@
+import Accordion from './components/Accordion';
+
+function AccordionPage() {
+  const items = [
+    {
+      id: 'asdfasd',
+      label: 'this is a label',
+      content: 'This is content',
+    },
+    {
+      id: 'fghhr6',
+      label: 'this is a label 2',
+      content: 'This is content 2',
+    },
+    {
+      id: '7rtygh',
+      label: 'this is a label 3',
+      content: 'This is content 3',
+    },
+  ];
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>Components</h1>
+      <div className='default'>
+        <Accordion items={items} />
+      </div>
+    </>
+  );
+}
+
+export default AccordionPage;
